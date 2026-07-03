f473aa592d93c69c2188bbd1f10833e8d6e70efe
titvvn <dat30294.email@gmail.com>
2026-07-03 22:30:51 +0700

    them2_buoi2

--- khu-dan-cu/src/App.vue
+++ khu-dan-cu/src/App.vue
@@ -1,13 +1,26 @@
 <script setup>
-  import {ref} from 'vue'
+  import {ref , computed } from 'vue'
   const soHo = ref(10)
   function themHo() {
     soHo.value++
-
   }
+  const danhSachHo = ref([
+    {
+      id: 1, ten: 'Nguyễn Tiến Đạt', phong: '101'
+    },
+    {
+      id: 2, ten: 'Trần Thị Lan', phong: '102'
+    },
+    {
+      id: 3, ten: 'Lê Văn Hùng', phong: '103'
+    }
+  ])
   function botHo(){
     if(soHo.value > 0)
-    soHo.value--
+  {
+     soHo.value--
+  }
+   
   }
 </script>
 
@@ -17,6 +30,9 @@
   <button @click="themHo"> Thêm Hộ Dân</button>
   <br>
   <button @click="botHo"> Bớt Hộ Dân</button>
+  <li v-for="ho in danhSachHo" :key="ho.id">
+    {{ ho.ten }} - Phòng {{ ho.phong }}
+  </li>
 </template>
 
 <style scoped></style>
