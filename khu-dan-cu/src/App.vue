e78fcaf9eae0d15950e0e8fc42083ab0897213b0
titvvn <dat30294.email@gmail.com>
2026-07-03 17:59:04 +0700

    buoi_1

---
+++ khu-dan-cu/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import {ref} from 'vue'
+  const soHo = ref(10)
+  function themHo() {
+    soHo.value++
+
+  }
+  function botHo(){
+    if(soHo.value > 0)
+    soHo.value--
+  }
+</script>
+
+<template>
+  <h1> Quản lý khu dân cư </h1>
+  <p> Tổng số hộ: {{ soHo }}</p>
+  <button @click="themHo"> Thêm Hộ Dân</button>
+  <br>
+  <button @click="botHo"> Bớt Hộ Dân</button>
+</template>
+
+<style scoped></style>
