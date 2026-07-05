74d02b1d6d0aea70ea2bcd26f318e5f78c1d9844
titvvn <dat30294.email@gmail.com>
2026-07-05 18:12:16 +0700

    Buoi3

--- khu-dan-cu/src/App.vue
+++ khu-dan-cu/src/App.vue
@@ -1,38 +1,58 @@
 <script setup>
-  import {ref , computed } from 'vue'
-  const soHo = ref(10)
+  import {ref , computed} from 'vue'
+  import HoDanItem from './components/HoDanItem.vue'
+  const tenMoi = ref('')
+  const soHo = computed(() => {
+    return danhSachHo.value.length
+  })
+  
   function themHo() {
-    soHo.value++
+    if(tenMoi.value.trim() === '') {
+      alert('Vui lòng nhập tên hộ dân!')
+      return
+    }
+    danhSachHo.value.push({
+      id: Date.now(),
+      ten: tenMoi.value,
+      phong: (danhSachHo.value.length + 1).toString()
+    })
+    tenMoi.value =''
+
+  }
+  function doiTrangThai(id) {
+    const ho = danhSachHo.value.find(h => h.id === id)
+    if(!ho) return
+    if(ho.tth === 'Đã đóng phí') {
+      ho.tth = 'Chưa đóng phí'
+    } else {
+      ho.tth = 'Đã đóng phí'
+    }
   }
   const danhSachHo = ref([
     {
-      id: 1, ten: 'Nguyễn Tiến Đạt', phong: '101'
+      id: 1, ten: 'Nguyễn Tiến Đạt', phong: '101',tth :'Đã đóng phí'
     },
     {
-      id: 2, ten: 'Trần Thị Lan', phong: '102'
+      id: 2, ten: 'Trần Thị Lan', phong: '102',tth :'Chưa đóng phí'
     },
     {
-      id: 3, ten: 'Lê Văn Hùng', phong: '103'
+      id: 3, ten: 'Lê Văn Hùng', phong: '103',tth :'Đã đóng phí'
     }
   ])
-  function botHo(){
-    if(soHo.value > 0)
-  {
-     soHo.value--
-  }
-   
+  function xoaHo(id){
+     danhSachHo.value = danhSachHo.value.filter(ho => ho.id !== id)
   }
 </script>
 
 <template>
   <h1> Quản lý khu dân cư </h1>
   <p> Tổng số hộ: {{ soHo }}</p>
+  
   <button @click="themHo"> Thêm Hộ Dân</button>
   <br>
-  <button @click="botHo"> Bớt Hộ Dân</button>
-  <li v-for="ho in danhSachHo" :key="ho.id">
-    {{ ho.ten }} - Phòng {{ ho.phong }}
-  </li>
+  <input v-model="tenMoi" placeholder="Nhập tên hộ">
+  <HoDanItem v-for ="ho in danhSachHo" :key="ho.id" :ho="ho" @xoa="xoaHo" @doitt="doiTrangThai"/>
+  <p v-if="soHo === 0"> Không có dân cư nào</p>
 </template>
 
 <style scoped></style>
---
+++ khu-dan-cu/src/components/HoDanItem.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const props = defineProps({
+        ho : Object
+    })
+    const emit = defineEmits(['xoa', 'doitt'])
+</script>
+<template>
+    <li>
+         {{ ho.ten }} - Phòng {{ ho.phong }} - {{ ho.tth }}
+        <button @click="emit('xoa', ho.id)">Xoá Hộ</button>
+        <button @click="emit('doitt',ho.id)">Đổi trạng thái</button>
+    </li>  
+</template>
+<style scoped></style>
